9bd95e61116853c7f6ba503788f17da1f998da75
בר קזז <pankerdorian@gmail.com>
2022-06-13 20:40:36 +0300

    tested get_status(drone)

--- drone_control.py
+++ drone_control.py
@@ -4,37 +4,25 @@ import matplotlib.pyplot as plt
 import cv2
 import threading
 
-
-def get_frame(tello):
-    tello.streamon()  # start UDP stream
-    while True:
-        frame_obj = tello.get_frame_read()
-        frame = frame_obj.frame
-        cv2.imshow('frame', frame)
-        cv2.waitKey(1)
-        if time.time() - start > 60:
-            print('stopping after 60 seconds of stream')
-            break
-
-
-def flip_drone(tello):
+# TODO: drone path 
+def drone_path():
     tello.takeoff()
-    time.sleep(10)
-    tello.flip_forward()
-    time.sleep(10)
-    tello.flip_forward()
+    time.sleep(5)
+    tello.move_forward(100)
     tello.land()
 
+def get_status(tello):
+    status = {
+        'battery': tello.get_battery(), 
+        'height': tello.get_height(), 
+        'speed': f'x: {tello.get_speed_x()} y: {tello.get_speed_y()} z: {tello.get_speed_z()}'
+    }
+    return status
 
-try:
-    tello = Tello()
-    tello.connect()
-    start = time.time()
-    print('start: ' + str(start))
-    print('battery: ' + str(tello.get_battery()))
-    # t = threading.Thread(target=flip_drone, args=[tello])
+# TODO:stream on 
+def stream_on():
     tello.streamon()  # start UDP stream
-    # t.start()
+    t.start()
     while True:
         frame_obj = tello.get_frame_read()
         frame = frame_obj.frame
@@ -43,13 +31,73 @@ try:
         if time.time() - start > 60:
             print('stopping after 60 seconds of stream')
             break
-    # t.join()
-except Exception as e:
-    print(f'error: {e}')
-finally:
-    tello.streamoff()
-    end = time.time()
-    print(f'end: {end}')
-    tello.end()
 
+def main():
+    tello = Tello()
+    tello.connect()
+    try:
+        status = get_status(tello)
+        print(status)
+    except Exception as e:
+        print(f'Exception raised: {e}')
+    finally:
+        tello.end()
+
+
+
+# instruction = ['forward' , 100]
+# route = [inst1, inst2, inst3]
+# goThroughRoute(route)
+
+
+# def get_frame(tello):
+#     tello.streamon()  # start UDP stream
+#     while True:
+#         frame_obj = tello.get_frame_read()
+#         frame = frame_obj.frame
+#         cv2.imshow('frame', frame)
+#         cv2.waitKey(1)
+#         if time.time() - start > 60:
+#             print('stopping after 60 seconds of stream')
+#             break
+
+
+# def flip_drone(tello):
+#     tello.takeoff()
+#     time.sleep(10)
+#     tello.flip_forward()
+#     time.sleep(10)
+#     tello.flip_forward()
+#     tello.land()
+
+
+# try:
+#     tello = Tello()
+#     tello.connect()
+#     start = time.time()
+#     print('start: ' + str(start))
+#     print('battery: ' + str(tello.get_battery()))
+#     # t = threading.Thread(target=flip_drone, args=[tello])
+#     tello.streamon()  # start UDP stream
+#     # t.start()
+#     while True:
+#         frame_obj = tello.get_frame_read()
+#         frame = frame_obj.frame
+#         cv2.imshow('frame', frame)
+#         cv2.waitKey(1)
+#         if time.time() - start > 60:
+#             print('stopping after 60 seconds of stream')
+#             break
+#     # t.join()
+# except Exception as e:
+#     print(f'error: {e}')
+# finally:
+#     tello.streamoff()
+#     end = time.time()
+#     print(f'end: {end}')
+#     tello.end()
+
+
+if __name__ == '__main__':
+    main()
 
